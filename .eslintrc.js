06aacbe9170d0c78f0b271d0465e928d5ddcaa55
Antony Giomar <antonygiomarx@gmail.com>
2023-05-29 21:45:58 -0600

    feat: added ignore directories :sparkles:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: "airbnb-typescript-prettier",
-
+  ignorePatterns: ["dist", "node_modules"],
   rules: {
     "import/prefer-default-export": "off",
     "no-useless-constructor": "off",
